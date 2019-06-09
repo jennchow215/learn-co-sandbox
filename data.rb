64f6524664ddaade9ac38b424eddadcef27abcd9
Jenn <jennchow215@gmail.com>
2019-06-09 18:33:32 +0000

    Automatically backed up by Learn

---
+++ data.rb
@@ -0,0 +1,11 @@
+def greet_instructor (name)
+  puts "Your name is "+name
+  return name
+end
+
+excited_teacher=greet_instructor ("Trevor") + "!!!!!"
+def full_name(first, init,last)
+  puts first+ " " +init + "." + last
+end
+jenn=full_name ("Jenn", "T", "Chow")
+puts Jenn
---
+++ music_library.rb
@@ -0,0 +1,18 @@
+music_library.each do |artist_array| = [
+  ["Adele",
+    ["19",
+      ["Day Dreamer", "Best for Last"]
+    ],
+    ["21",
+      ["Rolling in the Deep", "Rumor Has It"]
+    ]
+  ],
+  ["Beyonce",
+    ["4",
+      ["1 + 1", "Countdown"]
+    ],
+    ["Beyonce",
+      ["Haunted", "Pretty Hurts"]
+    ]
+  ]
+]
